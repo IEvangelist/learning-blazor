5bad346ddecbb8734065c5aaa58ba0a9489ec219
David Pine <david.pine@microsoft.com>
2021-06-22 15:22:34 -0500

    Pull in tailwind.css and drop bootstrap

---
+++ src/Web/postcss.config.js
@@ -0,0 +1,7 @@
+﻿module.exports = ({ env }) => ({
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+        cssnano: env === "production" ? { preset: "default" } : false
+    }
+});
---
+++ src/Web/tailwind.config.js
@@ -0,0 +1,20 @@
+﻿const colors = require('tailwindcss/colors')
+
+module.exports = {
+    mode: 'jit',
+    purge: [
+        './wwwroot/index.html',
+        './**/*.razor',
+    ],
+    darkMode: 'media',
+    theme: {
+        extend: {
+            colors: {
+                cyan: colors.cyan
+            }
+        },
+    },
+    variants: {
+        extend: {},
+    }
+}
