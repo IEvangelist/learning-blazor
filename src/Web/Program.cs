69e0648cacade13c6edc58bab4c9318d395c4101
David Pine <david.pine@microsoft.com>
2021-06-22 22:25:41 -0500

    Now jokes are flowing...

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Learning.Blazor.Extensions;
 
 namespace Learning.Blazor
 {
@@ -15,7 +16,7 @@ namespace Learning.Blazor
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+            builder.Services.AddClientServices();
             builder.Services.AddMsalAuthentication(options =>
             {
                 builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
