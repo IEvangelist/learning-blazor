a6049f6da1ed5956e1f0a958badb4f24877b3d5d
David Pine <david.pine@microsoft.com>
2021-09-30 22:26:55 -0500

    Attempt a few more fixes for the evening....

--- src/Web/Localization/CoalescingStringLocalizer.cs
+++ src/Web/Localization/CoalescingStringLocalizer.cs
@@ -3,65 +3,66 @@
 
 using Microsoft.Extensions.Localization;
 
-namespace Learning.Blazor.Localization;
-
-internal class CoalescingStringLocalizer<T>
+namespace Learning.Blazor.Localization
 {
-    private readonly IStringLocalizer<T> _localizer = null!;
-    private readonly IStringLocalizer<SharedResource> _sharedLocalizer = null!;
-    private readonly ILogger<T> _logger = null!;
-
-    public CoalescingStringLocalizer(
-        IStringLocalizer<T> localizer,
-        IStringLocalizer<SharedResource> sharedLocalizer,
-        ILogger<T> logger) =>
-        (_localizer, _sharedLocalizer, _logger) = (localizer, sharedLocalizer, logger);
-
-    /// <summary>
-    /// Gets the localized content for the current subcomponent,
-    /// relying on the contextually appropriate <see cref="IStringLocalizer{T}"/> implementation.
-    /// </summary>
-    internal LocalizedString this[string name]
+    internal class CoalescingStringLocalizer<T>
     {
-        get
+        private readonly IStringLocalizer<T> _localizer = null!;
+        private readonly IStringLocalizer<SharedResource> _sharedLocalizer = null!;
+        private readonly ILogger<T> _logger = null!;
+
+        public CoalescingStringLocalizer(
+            IStringLocalizer<T> localizer,
+            IStringLocalizer<SharedResource> sharedLocalizer,
+            ILogger<T> logger) =>
+            (_localizer, _sharedLocalizer, _logger) = (localizer, sharedLocalizer, logger);
+
+        /// <summary>
+        /// Gets the localized content for the current subcomponent,
+        /// relying on the contextually appropriate <see cref="IStringLocalizer{T}"/> implementation.
+        /// </summary>
+        internal LocalizedString this[string name]
         {
-            var localizedString = _localizer[name]
-                ?? _sharedLocalizer[name]
-                ?? new(name, name);
+            get
+            {
+                var localizedString = _localizer[name]
+                    ?? _sharedLocalizer[name]
+                    ?? new(name, name);
 
-            LogIfNotFound(localizedString);
+                LogIfNotFound(localizedString);
 
-            return localizedString;
+                return localizedString;
+            }
         }
-    }
 
-    /// <summary>
-    /// Gets the localized content for the current subcomponent,
-    /// relying on the contextually appropriate <see cref="IStringLocalizer{T}"/> implementation.
-    /// </summary>
-    internal LocalizedString this[string name, params object[] arguments]
-    {
-        get
+        /// <summary>
+        /// Gets the localized content for the current subcomponent,
+        /// relying on the contextually appropriate <see cref="IStringLocalizer{T}"/> implementation.
+        /// </summary>
+        internal LocalizedString this[string name, params object[] arguments]
         {
-            var localizedString = _localizer[name, arguments]
-                ?? _sharedLocalizer[name, arguments]
-                ?? new(name, name);
+            get
+            {
+                var localizedString = _localizer[name, arguments]
+                    ?? _sharedLocalizer[name, arguments]
+                    ?? new(name, name);
 
-            LogIfNotFound(localizedString);
+                LogIfNotFound(localizedString);
 
-            return localizedString;
+                return localizedString;
+            }
         }
-    }
 
-    void LogIfNotFound(LocalizedString localizedString)
-    {
-        if (localizedString is { ResourceNotFound: true })
+        void LogIfNotFound(LocalizedString localizedString)
         {
-            _logger.LogInformation(
-                "Unable to find {Name}, searched in {Location} - using {Value}.",
-                localizedString.Name,
-                localizedString.SearchedLocation,
-                localizedString.Value);
+            if (localizedString is { ResourceNotFound: true })
+            {
+                _logger.LogInformation(
+                    "Unable to find {Name}, searched in {Location} - using {Value}.",
+                    localizedString.Name,
+                    localizedString.SearchedLocation,
+                    localizedString.Value);
+            }
         }
     }
 }
--- src/Web/Shared/LoginDisplay.razor.cs
+++ src/Web/Shared/LoginDisplay.razor.cs
@@ -1,9 +1,19 @@
 ﻿// Copyright (c) 2021 David Pine. All rights reserved.
 // Licensed under the MIT License.
 
+using Microsoft.AspNetCore.Components.Web;
+
 namespace Learning.Blazor.Shared
 {
     public partial class LoginDisplay
     {
+        void OnLogIn(MouseEventArgs args) =>
+            Navigation.NavigateTo("authentication/login", true);
+
+        async Task OnLogOut(MouseEventArgs args)
+        {
+            await SignOutManager.SetSignOutState();
+            Navigation.NavigateTo("authentication/logout");
+        }
     }
 }
