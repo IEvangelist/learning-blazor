d0cba5e9b7be65035e8f03359099df5d52126e5c
David Pine <david.pine@microsoft.com>
2021-11-10 20:11:54 -0600

    Tweaks and movement

---
+++ src/Web/Components/IntroductionComponent.razor.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) 2021 David Pine. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Extensions.Localization;
+
+namespace Learning.Blazor.Components
+{
+    public partial class IntroductionComponent
+    {
+        private LocalizedString? _intro => localize["ThankYou"];
+    }
+}
