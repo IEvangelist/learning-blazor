07a14bc39418c4f259d88e3b48b2aef040d93fbf
David Pine <david.pine@microsoft.com>
2022-02-17 21:27:40 -0600

    Oops, terminate it all

--- src/Web.Client/Extensions/WebAssemblyHostBuilderExtensions.cs
+++ src/Web.Client/Extensions/WebAssemblyHostBuilderExtensions.cs
@@ -10,7 +10,7 @@ internal static class WebAssemblyHostBuilderExtensions
     {
         var (services, configuration) =
             (builder.Services, builder.Configuration);
-        ;
+
         services.AddScoped<ApiAccessAuthorizationMessageHandler>();
         services.Configure<WebApiOptions>(
             configuration.GetSection(nameof(WebApiOptions)));
