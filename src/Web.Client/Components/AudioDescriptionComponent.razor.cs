326b19ce9c5956985e44782748346145fdc99556
David Pine <david.pine@microsoft.com>
2022-03-14 23:20:34 -0500

    Encapsulate the audio description modal into its own component.

--- src/Web.Client/Components/AudioDescriptionComponent.razor.cs
+++ src/Web.Client/Components/AudioDescriptionComponent.razor.cs
@@ -12,16 +12,27 @@ namespace Learning.Blazor.Components
         private string _voice = "Auto";
         private double _voiceSpeed = 1;
 
-        private ModalComponent _modal = null!;
+        private AudioDescriptionModalComponent _modal = null!;
+        private AudioDescriptionDetails _details;
 
-        protected override async Task OnInitializedAsync()
+        protected override async Task OnAfterRenderAsync(bool firstRender)
         {
-            (_voice, _voiceSpeed) =
-                AppState.ClientVoicePreference;
+            if (firstRender)
+            {
+                (_voice, _voiceSpeed) =
+                    AppState.ClientVoicePreference;
 
-            await UpdateClientVoices(
-                await JavaScript.GetClientVoices(
-                    this, nameof(UpdateClientVoices)));
+                _details = new AudioDescriptionDetails(
+                    AppState,
+                    _voiceSpeeds,
+                    _voices,
+                    _voice,
+                    _voiceSpeed);
+
+                await UpdateClientVoices(
+                    await JavaScript.GetClientVoices(
+                        this, nameof(UpdateClientVoices)));
+            }
         }
 
         [JSInvokable]
@@ -31,32 +42,35 @@ namespace Learning.Blazor.Components
                 var voices = voicesJson.FromJson<List<SpeechSynthesisVoice>>();
                 if (voices is { Count: > 0 })
                 {
-                    _voices = voices;
+                    _details = _details with
+                    {
+                        Voices = _voices = voices
+                    };
 
                     StateHasChanged();
                 }
             });
 
-        private void OnVoiceSpeedChange(ChangeEventArgs args) =>
-            _voiceSpeed = double.TryParse(
-                args?.Value?.ToString() ?? "1", out var speed) ? speed : 1;
-
         private async Task ShowAsync() => await _modal.ShowAsync();
 
-        private async Task ConfirmAsync()
+        private void OnDetailsSaved(AudioDescriptionDetails details)
         {
+            // Noop...
+            _details = details with { };
+
             AppState.ClientVoicePreference =
-                new ClientVoicePreference(_voice, _voiceSpeed);
+                new ClientVoicePreference(_details.Voice, _details.VoiceSpeed);
 
             Logger.LogInformation(
                 "There are {Length} item in localStorage.", LocalStorage.Length);
 
-            await _modal.ConfirmAsync();
         }
-
-        private void OnDismissed(DismissalReason reason) =>
-            Logger.LogWarning(
-                "User '{Reason}' the audio description modal.",
-                reason);
     }
+
+    public readonly record struct AudioDescriptionDetails(
+        AppInMemoryState AppState,
+        IList<double> VoiceSpeeds,
+        IList<SpeechSynthesisVoice> Voices,
+        string Voice,
+        double VoiceSpeed);
 }
---
+++ src/Web.Client/Components/AudioDescriptionModalComponent.razor.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) 2021 David Pine. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Learning.Blazor.Components
+{
+    public sealed partial class AudioDescriptionModalComponent
+    {
+        [Parameter, EditorRequired]
+        public AudioDescriptionDetails Details { get; set; }
+
+        [Parameter, EditorRequired]
+        public string Title { get; set; } = null!;
+
+        [Parameter, EditorRequired]
+        public EventCallback<AudioDescriptionDetails> OnDetailsSaved { get; set; }
+
+        private string _voice = "Auto";
+        private ModalComponent _modal = null!;
+
+        private void OnVoiceSpeedChange(ChangeEventArgs args) =>
+            Details = Details with
+            {
+                VoiceSpeed = double.TryParse(
+                    args?.Value?.ToString() ?? "1", out var speed) ? speed : 1
+            };
+
+        internal async Task ShowAsync() => await _modal.ShowAsync();
+
+        internal async Task ConfirmAsync()
+        {
+            if (OnDetailsSaved.HasDelegate)
+            {
+                await OnDetailsSaved.InvokeAsync(
+                    Details = Details with { Voice = _voice });
+            }
+
+            await _modal.ConfirmAsync();
+        }
+    }
+}
