5e130975db72e2481c692912288ae935442d14b2
David Pine <david.pine@microsoft.com>
2022-04-26 12:09:59 -0500

    Playwright (#45)
    * WIP adding playwright
    
    * Another wip update
    
    * Tweak
    
    * Minor fix
    
    * Work in progress.

--- tests/Web.Client.EndToEndTests/GlobalUsings.cs
+++ tests/Web.Client.EndToEndTests/GlobalUsings.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) 2021 David Pine. All rights reserved.
 // Licensed under the MIT License.
 
-global using Blazor.Serialization.Extensions;
-global using System.Security.Claims;
 global using Xunit;
+global using Microsoft.Playwright;
+global using static System.Environment;
---
+++ tests/Web.Client.EndToEndTests/LoginToApp.cs
@@ -0,0 +1,71 @@
+// Copyright (c) 2021 David Pine. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Web.Client.EndToEndTests;
+
+public sealed partial class LoginToApp
+{
+    const string LearningBlazorSite = "https://webassemblyof.net";
+    const string LearningBlazorB2CSite = "https://learningblazor.b2clogin.com";
+
+    static IBrowserType ToBrowser(string browser, IPlaywright pw) =>
+        browser switch { "chromium" => pw.Chromium, "firefox" => pw.Firefox, _ => pw.Webkit };
+
+    static Credentials GetCredentials() =>
+        new(
+            GetEnvironmentVariable("TEST_USERNAME"),
+            GetEnvironmentVariable("TEST_PASSWORD"));
+
+    readonly record struct Credentials(string? Username, string? Password);
+
+    [
+        Theory(Skip = "The changes to the element id's has not yet been deployed."),
+        InlineData("chromium"),
+        InlineData("firefox"),
+        InlineData("webkit"),
+    ]
+    public async Task CanLoginWithVerifiedCredentials(string browserName)
+    {
+        var (username, password) = GetCredentials();
+        Assert.NotNull(username);
+        Assert.NotNull(password);
+
+        using var playwright = await Playwright.CreateAsync();
+        await using var sut = await ToBrowser(browserName, playwright)
+            .LaunchAsync(new()
+            {
+                Headless = false
+            });
+
+        await using var context = await sut.NewContextAsync(
+            new()
+            {
+                Permissions = new[] { "geolocation" },
+                Geolocation = new Geolocation() // Milwaukee, WI
+                {
+                    Latitude = 43.04181f,
+                    Longitude = -87.90684f
+                }
+            });
+
+        var loginPage = await context.NewPageAsync();
+        await loginPage.RunAndWaitForNavigationAsync(async () =>
+        {
+            await loginPage.GotoAsync(LearningBlazorSite);
+        },
+        new()
+        {
+            UrlString = $"{LearningBlazorB2CSite}/**",
+            WaitUntil = WaitUntilState.NetworkIdle
+        });
+
+        // Enter our test credentals, and attempt "sign in".
+        await loginPage.FillAsync("#email", username ?? "fail");
+        await loginPage.FillAsync("#password", password ?? "?!?!");
+        await loginPage.ClickAsync("#next" /* "Sign in" button */);
+
+        var selector = "#weather-city-state";
+        var text = await loginPage.Locator(selector).InnerTextAsync();
+        Assert.Equal("Milwaukee, Wisconsin (US)", text);
+    }
+}
